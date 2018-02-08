38c7e99d79e604d7cf151fd52f84990c3e4ea82a
claudiocz <claudiocanedocourse@gmail.com>
2018-02-08 13:31:08 -0400

    Clase VentanaPlacas creado

---
+++ src/com/placarestringidas/VentanaPlacas.java
@@ -0,0 +1,48 @@
+package com.placarestringidas;
+
+import java.util.Calendar;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+
+public class VentanaPlacas extends JFrame {
+    private JLabel etiqueta1, etiqueta2;
+    private JTextField campo;
+    private JButton validaPlaca;
+    private JDialog verificacion;
+
+    public VentanaPlacas() throws HeadlessException {
+        super("Verificacion de circulacion de Placas");// titulo de la ventana
+        etiqueta1 = new JLabel("Ingrese Numero de Placa");
+        campo = new JTextField(10);
+        validaPlaca = new JButton("Verificar");
+
+        verificacion = new JDialog(this);
+        verificacion.setSize(400, 250);
+        etiqueta2 = new JLabel(" ");
+
+        FlowLayout diseño = new FlowLayout();
+        setLayout(diseño);
+
+        add(etiqueta1);
+        add(campo);
+        add(validaPlaca);
+
+        verificacion.add(etiqueta2);
+
+        validaPlaca.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                validarNumPlaca();
+            }
+        });
+
+    }
+
+
+}
+
+
+
